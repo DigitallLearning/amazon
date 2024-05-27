2b5813b61c35cee315e2da802ddae647acd8f959
digitlalearning <digitallearning1505@gmail.com>
2024-05-27 13:28:39 +0530

    new

--- index.js
+++ index.js
@@ -43,4 +43,18 @@ app.get("/",async(req,resp)=>{
     const data=await pSchema.find()
     resp.send(data)
 })
+app.get("/:key",async(req,resp)=>{
+  // resp.send(req.params.key)
+   const data=await pSchema.find({pid:req.params.key})
+   resp.send(data)
+})
+app.get("/search/:key",async(req,resp)=>{
+     //resp.send(req.params.key)
+     const data=await pSchema.find({
+        "$or":[
+            {"pcat":{$regex:req.params.key}}
+        ]
+     })
+     resp.send(data)
+  })
 app.listen(4000)
